2b66318aebefe80ac6a50f8517cbe59a1de842cc
filipevsco <filipev@me.com>
2023-05-23 19:25:39 -0300

    link about

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from blogapp import views
 
+app_name = 'blogapp'
+
 urlpatterns = [
     path('', views.index, name="index"),
+    path('/about/', views.about, name="about"),
 ]
