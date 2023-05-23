2214bf37ee62d612fd9594b87391e6d28bd22197
filipevsco <filipev@me.com>
2023-05-23 20:13:06 -0300

    links ok

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -6,4 +6,8 @@ app_name = 'blogapp'
 urlpatterns = [
     path('', views.index, name="index"),
     path('/about/', views.about, name="about"),
+    path('/post/', views.post, name="post"),
+    path('/contact/', views.contact, name="contact"),
+
+
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -6,3 +6,9 @@ def index(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def post(request):
+    return render(request, 'post.html')
+
+def contact(request):
+    return render(request, 'contact.html')    
