2a0aa30d93c4da7cc1c804ee3be0131e4507f20e
filipevsco <filipev@me.com>
2023-05-28 18:33:30 -0300

    models

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,15 +1,17 @@
 from django.db import models
-
-# Create your models here.
-class Post(models.Model):
-    pass
+from users.models import CustomUser
 
 
 class Categoria(models.Model):
-    pass
+    nome = models.CharField(max_length=50, unique=True)
 
 
-class Tag(model.Model):
-    pass
-
-    
+class Post(models.Model):
+    titulo = models.Charfield(max_lengh=30, unique=True)
+    subtitulo = models.Charfield(max_length=40)
+    texto = models.TextField()
+    autor = models.ForeignKey(CustomUser, verbose_name=_(""), on_delete=models.CASCADE)
+    data = models.DateTimeField(auto_now_add=True)
+    # image = StdImageField(upload_to='static/img/', variations={'thumbnail': {"width": 300, "height": 400, "crop": True}}, blank=True, null=True)
+    #tag
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
