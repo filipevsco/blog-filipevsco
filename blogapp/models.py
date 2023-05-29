b82a8e5367ba25cbd5441180e8b42ad5161571b7
Filipe Vasconcelos <filipev@me.com>
2023-05-29 10:44:43 -0300

    Save models.py.

--- blogapp/models.py
+++ blogapp/models.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 class Categoria(models.Model):
     nome = models.CharField(max_length=50, unique=True)
 
+    def __str__(self):
+        return self.nome
 
 class Post(models.Model):
     titulo = models.CharField(max_length=30, unique=True)
